bf0b67ab2c830577873232ae2a2c0c3fd39addc7
Camila <clee@itba.edu.ar>
2024-11-17 02:15:28 -0300

    added top app bar

--- app/src/main/java/com/example/wall_et_mobile/TopAppBar.kt
+++ app/src/main/java/com/example/wall_et_mobile/TopAppBar.kt
@@ -1,9 +1,8 @@
 package com.example.wall_et_mobile
-
-import android.util.Log
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,44 +11,50 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.wall_et_mobile.model.User
 import com.example.wall_et_mobile.ui.theme.WallEtTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomTopAppBar() {
+fun CustomTopAppBar(user: User) {
     TopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary,
             titleContentColor = MaterialTheme.colorScheme.onPrimary,
         ),
-        title = {
-            Text("Small Top App Bar")
-        },
         navigationIcon = {
-            IconButton(onClick = { Log.d("TAG", "CustomTopAppBar: ") }) {
+            IconButton(onClick = {  }) {
                 Icon(
-                    Icons.Rounded.ShoppingCart,
-                    contentDescription = "hi"
+                    Icons.Filled.Person,
+                    contentDescription = "Avatar",
+                    modifier = Modifier.size(24.dp),
+                    tint = MaterialTheme.colorScheme.onPrimary
                 )
             }
-            // Navigation icon
         },
         actions = {
             IconButton(onClick = { /* do something */ }) {
                 Icon(
-                    imageVector = Icons.Filled.Menu,
-                    contentDescription = "Localized description"
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "Localized description",
+                    tint = MaterialTheme.colorScheme.onPrimary
                 )
             }
         },
+        title = {
+            Text(user.name, style = MaterialTheme.typography.titleSmall)
+        }
     )
 }
 
 @Preview
 @Composable
 fun PreviewTopAppBar() {
+    val testUser = User(1, "test", "test", "Lauti", "test", "test", "test", "test")
     WallEtTheme {
-        CustomTopAppBar()
+        CustomTopAppBar(testUser)
     }
 }
