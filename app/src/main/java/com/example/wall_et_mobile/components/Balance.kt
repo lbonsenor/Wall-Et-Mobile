1674326805b8013db14948eaf8a15a314495c141
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2024-11-16 21:35:05 -0300

    Center balance

--- app/src/main/java/com/example/wall_et_mobile/components/Balance.kt
+++ app/src/main/java/com/example/wall_et_mobile/components/Balance.kt
@@ -5,8 +5,10 @@ import android.icu.util.CurrencyAmount
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Colors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.Text
@@ -16,7 +18,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import java.text.NumberFormat
@@ -30,62 +34,63 @@ fun Balance(balance: CurrencyAmount = CurrencyAmount(123000.0, Currency.getInsta
 
     val (isShown, setShown) = remember { mutableStateOf(false) }
 
-
-
-        Row (
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
-                .padding(end = 30.dp),
-        ){
-//            Text(
-//                text = stringResource(R.string.balance),
-//                color = MaterialTheme.colorScheme.onPrimary,
-//                style = MaterialTheme.typography.labelMedium
-//            )
-            Column (
-                modifier = Modifier.weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally
+    Row (
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom,
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 10.dp)
+    ){
+        Spacer(modifier = Modifier.weight(1/6f))
+        Column (
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.balance),
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.labelMedium
+            )
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                Row (verticalAlignment = Alignment.Top) {
-                    if (isShown) {
-                        Text(
-                            text = whole,
-                            color = MaterialTheme.colorScheme.onPrimary,
-                            style = MaterialTheme.typography.displayMedium,
-                            fontFamily = DarkerGrotesque,
-                            fontWeight = FontWeight.Black
-                        )
-                        Text(
-                            text = decimal,
-                            color = MaterialTheme.colorScheme.onPrimary,
-                            style = MaterialTheme.typography.bodyMedium,
-                            fontFamily = DarkerGrotesque,
-                            fontWeight = FontWeight.Black,
-                            modifier = Modifier.padding(3.dp, 5.dp, 0.dp, 0.dp)
-                        )
-                    } else {
-                        Text(
-                            text = "$******",
-                            color = MaterialTheme.colorScheme.onPrimary,
-                            style = MaterialTheme.typography.displayMedium,
-                            fontFamily = DarkerGrotesque,
-                            fontWeight = FontWeight.Black,
-                        )
-                    }
-
+                if (isShown) {
+                    Text(
+                        text = whole,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        style = MaterialTheme.typography.displayMedium,
+                        fontFamily = DarkerGrotesque,
+                        fontWeight = FontWeight.Black
+                    )
+                    Text(
+                        text = ".$decimal",
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontFamily = DarkerGrotesque,
+                        fontWeight = FontWeight.Black,
+                        modifier = Modifier.padding(start = 5.dp)
+                    )
+                } else {
+                    Text(
+                        text = "$******",
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        style = MaterialTheme.typography.displayMedium,
+                        fontFamily = DarkerGrotesque,
+                        fontWeight = FontWeight.Black,
+                    )
                 }
-            }
-            IconToggleButton(
-                checked = isShown,
-                onCheckedChange = { setShown(!isShown) }
-            ) {
-                Icon(
-                    painter = if (isShown) painterResource(R.drawable.visibility) else painterResource(R.drawable.visibility_off),
-                    contentDescription = "Show Balance",
-                    tint = MaterialTheme.colorScheme.onPrimary
-                )
+
             }
         }
+        IconToggleButton(
+            checked = isShown,
+            onCheckedChange = { setShown(!isShown) },
+        ) {
+            Icon(
+                painter = if (isShown) painterResource(R.drawable.visibility) else painterResource(R.drawable.visibility_off),
+                contentDescription = "Show Balance",
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+        }
     }
+}
 
