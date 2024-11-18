f14c1d3b6cb33ed777403fa666548ffe5b24f397
Camila <clee@itba.edu.ar>
2024-11-18 20:12:22 -0300

    restored peace on earth

--- app/src/main/java/com/example/wall_et_mobile/MainActivity.kt
+++ app/src/main/java/com/example/wall_et_mobile/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.wall_et_mobile
 
+//noinspection UsingMaterialAndMaterial3Libraries
+//noinspection UsingMaterialAndMaterial3Libraries
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,38 +9,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.example.wall_et_mobile.ui.theme.WallEtTheme
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.FabPosition
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Scaffold
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.example.wall_et_mobile.screens.ActivitiesScreen
-import com.example.wall_et_mobile.screens.HomeScreen
-import com.example.wall_et_mobile.model.Screen
-import com.example.wall_et_mobile.screens.CardsScreen
-import com.example.wall_et_mobile.screens.ForgotPasswordScreen
-import com.example.wall_et_mobile.screens.HomeScreenLandscape
-import com.example.wall_et_mobile.screens.LoginScreen
-import com.example.wall_et_mobile.screens.SignupScreen
-import com.example.wall_et_mobile.screens.transfer.SelectAmountScreen
-import com.example.wall_et_mobile.screens.transfer.SelectPaymentScreen
-import com.example.wall_et_mobile.screens.transfer.TransferScreen
+import com.example.wall_et_mobile.ui.theme.WallEtTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import androidx.compose.material3.Scaffold as Scaffold2
 
@@ -60,7 +46,7 @@ class MainActivity : ComponentActivity() {
             WallEtTheme {
                 val navController = rememberNavController()
 
-                when (orientation) {
+                when (orientation){
                     Configuration.ORIENTATION_PORTRAIT -> ScaffoldPortrait(navController, qrScanner)
                     else -> ScaffoldLandscape(navController, qrScanner)
                 }
@@ -71,86 +57,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ScaffoldPortrait(navController: NavHostController, qrScanner: QRScanner) {
+fun ScaffoldPortrait(navController: NavHostController, qrScanner: QRScanner){
     val systemUiController = rememberSystemUiController()
 
     val statusBarColor = MaterialTheme.colorScheme.background
     val systemNavColor = MaterialTheme.colorScheme.primary
-    val currentRoute = currentRoute(navController)
 
     SideEffect {
         systemUiController.setStatusBarColor(statusBarColor)
         systemUiController.setNavigationBarColor(systemNavColor)
     }
-    Scaffold(
+    Scaffold (
         //topBar = { CustomTopAppBar(User(1, "test", "test", "Lautaro", "test", "test", "test", "test",)) },
-        bottomBar = {
-            if (currentRoute != Screen.Login.route && currentRoute != Screen.Signup.route) {
-                CustomAppBar(navController)
-            }
-        },
+        bottomBar = { CustomAppBar(navController) },
         floatingActionButtonPosition = FabPosition.Center,
         isFloatingActionButtonDocked = true,
-        floatingActionButton = {
-            if (currentRoute != Screen.Login.route && currentRoute != Screen.Signup.route) {
-                QRFab(qrScanner::startScan)
-            }
-        },
+        floatingActionButton = { QRFab(qrScanner::startScan) },
         backgroundColor = MaterialTheme.colorScheme.background,
         modifier = Modifier.systemBarsPadding()
 
     ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = Screen.Login.route,
-            builder = {
-                composable(Screen.Login.route) { LoginScreen(navController) }
-                composable(Screen.Signup.route) { SignupScreen(navController) }
-                composable(Screen.ForgotPassword.route) { ForgotPasswordScreen(navController) }
-                composable(Screen.Home.route) { HomeScreen(innerPadding, navController) }
-                composable(Screen.Cards.route) { CardsScreen(innerPadding) }
-                composable(Screen.Activities.route) { ActivitiesScreen(innerPadding) }
-                composable(Screen.SeeMore.route) {}
-                composable(Screen.Transfer.route) { TransferScreen(innerPadding, navController) }
-                composable(
-                    route = Screen.SelectAmount.route,
-                    arguments = listOf(
-                        navArgument(name = "id") {
-                            type = NavType.IntType
-                        }
-                    )
-                ) { backStackEntry ->
-                    SelectAmountScreen(
-                        innerPadding,
-                        navController,
-                        backStackEntry.arguments?.getInt("id")!!
-                    )
-                }
-                composable(
-                    route = Screen.SelectPaymentMethod.route,
-                    arguments = listOf(
-                        navArgument(name = "id") {
-                            type = NavType.IntType
-                        },
-                        navArgument(name = "amount") {
-                            type = NavType.StringType
-                        }
-                    )
-                ) { backStackEntry ->
-                    SelectPaymentScreen(
-                        innerPadding,
-                        navController,
-                        backStackEntry.arguments?.getInt("id")!!,
-                        backStackEntry.arguments?.getString("amount")!!
-                    )
-                }
-            }
-        )
+        AppNavHost(innerPadding, modifier = Modifier, navController)
     }
 }
 
 @Composable
-fun ScaffoldLandscape(navController: NavHostController, qrScanner: QRScanner) {
+fun ScaffoldLandscape(navController: NavHostController, qrScanner: QRScanner){
     val systemUiController = rememberSystemUiController()
 
     val statusBarColor = MaterialTheme.colorScheme.primary
@@ -169,69 +101,9 @@ fun ScaffoldLandscape(navController: NavHostController, qrScanner: QRScanner) {
         )
         {
             NavBarLandscape(navController, qrScanner)
-            NavHost(
-                navController = navController,
-                startDestination = Screen.Home.route,
-                builder = {
-                    composable(Screen.Home.route) {
-                        HomeScreenLandscape(
-                            innerPadding,
-                            navController
-                        )
-                    }
-                    composable(Screen.Cards.route) { CardsScreen(innerPadding) }
-                    composable(Screen.Activities.route) { ActivitiesScreen(innerPadding) }
-                    composable(Screen.SeeMore.route) {}
-                    composable(Screen.Transfer.route) {
-                        TransferScreen(
-                            innerPadding,
-                            navController
-                        )
-                    }
-                    composable(
-                        route = Screen.SelectAmount.route,
-                        arguments = listOf(
-                            navArgument(name = "id") {
-                                type = NavType.IntType
-                            }
-                        )
-                    ) { backStackEntry ->
-                        SelectAmountScreen(
-                            innerPadding,
-                            navController,
-                            backStackEntry.arguments?.getInt("id")!!
-                        )
-                    }
-                    composable(
-                        route = Screen.SelectPaymentMethod.route,
-                        arguments = listOf(
-                            navArgument(name = "id") {
-                                type = NavType.IntType
-                            },
-                            navArgument(name = "amount") {
-                                type = NavType.StringType
-                            }
-                        )
-                    ) { backStackEntry ->
-                        SelectPaymentScreen(
-                            innerPadding,
-                            navController,
-                            backStackEntry.arguments?.getInt("id")!!,
-                            backStackEntry.arguments?.getString("amount")!!
-                        )
-                    }
-                }
-            )
+            LandscapeAppNavHost(innerPadding, modifier = Modifier, navController)
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 
