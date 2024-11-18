c20bd76dba6846b387505a4c8a81d37ce687d3ec
Camila <clee@itba.edu.ar>
2024-11-18 05:33:49 -0300

    optimized some imports

--- app/src/main/java/com/example/wall_et_mobile/components/TransferCardItem.kt
+++ app/src/main/java/com/example/wall_et_mobile/components/TransferCardItem.kt
@@ -1,39 +1,30 @@
 package com.example.wall_et_mobile.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.wall_et_mobile.R
 import com.example.wall_et_mobile.data.mock.MockCards
 import com.example.wall_et_mobile.model.CardBrand
 import com.example.wall_et_mobile.model.CardDetails
-import com.example.wall_et_mobile.ui.theme.DarkerGrotesque
-import com.example.wall_et_mobile.ui.theme.TransparentGray
 import com.example.wall_et_mobile.ui.theme.WallEtTheme
-import com.example.wall_et_mobile.ui.theme.White
 
 
 @Composable
@@ -51,8 +42,7 @@ fun TransferCardItem(card: CardDetails) {
         modifier = Modifier
             .aspectRatio(8 / 3f)
             .height(100.dp)
-            .padding(8.dp)
-        ,
+            .padding(8.dp),
     ) {
         Row(
             modifier = Modifier
--- app/src/main/java/com/example/wall_et_mobile/screens/transfer/SelectAmountScreen.kt
+++ app/src/main/java/com/example/wall_et_mobile/screens/transfer/SelectAmountScreen.kt
@@ -1,8 +1,6 @@
 package com.example.wall_et_mobile.screens.transfer
 
 
-import android.icu.util.Currency
-import android.icu.util.CurrencyAmount
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -20,16 +18,12 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -38,7 +32,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
