b35921ea9aa146c6252efa0c23c82428e4e145e9
Camila <clee@itba.edu.ar>
2024-11-21 21:30:53 -0300

    added to android manifest

--- app/src/main/java/com/example/wall_et_mobile/data/mock/MockCards.kt
+++ app/src/main/java/com/example/wall_et_mobile/data/mock/MockCards.kt
@@ -6,29 +6,29 @@ import com.example.wall_et_mobile.model.CardType
 
 object MockCards {
     val sampleCards = listOf(
-        CardDetails(
-            cardNumber = "4234 5678 9012 3456",
-            cardType = CardType.CREDIT_CARD,
-            cardHolder = "John Doe",
-            cardExpiration = "01/25",
-            cardCvv = "123",
-            gradient = CardGradient.PURPLE_INDIGO
-        ),
-        CardDetails(
-            cardNumber = "5876 5432 1098 7654",
-            cardType = CardType.DEBIT_CARD,
-            cardHolder = "Jane Smith",
-            cardExpiration = "03/29",
-            cardCvv = "456",
-            gradient = CardGradient.DARK_TEAL
-        ),
-        CardDetails(
-            cardNumber = "4131 5234 6213 1231",
-            cardType = CardType.DEBIT_CARD,
-            cardHolder = "Juan José",
-            cardExpiration = "12/31",
-            cardCvv = "442",
-            gradient = CardGradient.MIDNIGHT_BLUE
-        )
+//        CardDetails(
+//            cardNumber = "4234 5678 9012 3456",
+//            cardType = CardType.CREDIT_CARD,
+//            cardHolder = "John Doe",
+//            cardExpiration = "01/25",
+//            cardCvv ="123",
+//            gradient = CardGradient.PURPLE_INDIGO
+//        ),
+//        CardDetails(
+//            cardNumber = "5876 5432 1098 7654",
+//            cardType = CardType.DEBIT_CARD,
+//            cardHolder = "Jane Smith",
+//            cardExpiration = "03/29",
+//            cardCvv = "456",
+//            gradient = CardGradient.DARK_TEAL
+//        ),
+//        CardDetails(
+//            cardNumber = "4131 5234 6213 1231",
+//            cardType = CardType.DEBIT_CARD,
+//            cardHolder = "Juan José",
+//            cardExpiration = "12/31",
+//            cardCvv = "442",
+//            gradient = CardGradient.MIDNIGHT_BLUE
+//        )
     )
 }
--- app/src/main/java/com/example/wall_et_mobile/ui/home/WalletViewModel.kt
+++ app/src/main/java/com/example/wall_et_mobile/ui/home/WalletViewModel.kt
@@ -6,13 +6,12 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import coil3.network.HttpException
-import com.example.wall_et_mobile.model.RegisteredUser
-import com.example.wall_et_mobile.network.WalletApi
+import com.example.wall_et_mobile.data.model.User
 import kotlinx.coroutines.launch
 import okio.IOException
 
 sealed interface WalletUiState {
-    data class Success(val user: RegisteredUser) : WalletUiState
+    data class Success(val user: User) : WalletUiState
     data object Error : WalletUiState
     data object Loading : WalletUiState
 
